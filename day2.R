f58b1c0161e90ce37e52fc6cb66e921a0acf7c1d
mithunveluru <mithunveluru@gmail.com>
2025-03-11 16:29:04 +0530

    DAY2_RFile

---
+++ day2.R
@@ -0,0 +1,8 @@
+cars_data <- data.frame(
+  Vehicle = c("Chevrolet Camaro", "Dodge Neon", "Honda Accord", "Lincoln Continental", 
+              "Oldsmobile Aurora", "Pontiac Grand Am", "Mitsubishi Eclipse", 
+              "BMW 3-Series", "Honda Civic", "Toyota Camry", "Hyundai Accent", 
+              "Mazda Protege", "Cadillac DeVille"),
+  Weight = c(3.545, 2.6, 3.245, 3.93, 3.995, 3.115, 3.235, 3.225, 2.44, 3.24, 2.29, 2.5, 4.02),
+  Efficiency = c(30, 32, 30, 24, 26, 30, 33, 27, 37, 32, 37, 34, 26)
+)
