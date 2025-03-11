b13d73a1cb66c5777fdf9628a9dbddf190046604
mithunveluru <mithunveluru@gmail.com>
2025-03-11 16:35:57 +0530

    updation

--- day2.R
+++ day2.R
@@ -6,3 +6,7 @@ cars_data <- data.frame(
   Weight = c(3.545, 2.6, 3.245, 3.93, 3.995, 3.115, 3.235, 3.225, 2.44, 3.24, 2.29, 2.5, 4.02),
   Efficiency = c(30, 32, 30, 24, 26, 30, 33, 27, 37, 32, 37, 34, 26)
 )
+
+cov(cars_data$Weight, cars_data$Efficiency)
+cor.test(cars_data$Weight, cars_data$Efficiency)
+lm(cars_data$Weight ~ cars_data$Efficiency)
